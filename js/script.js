9179e385a038af8e2575f9392f6888ab25a51d9f
Alessandro <aleneri242@gmail.com>
2020-11-19 18:25:18 +0100

    aggiunta funzione

--- js/script.js
+++ js/script.js
@@ -9,15 +9,33 @@
 
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
+
+var arrayBombe = [];
+console.log(arrayBombe);
+
+for (var i = 1; i <= 16; i++) {
+  numeroCasuale = generaNumeri(1, 100);
+  arrayBombe.push(numeroCasuale);
+}
+
+
+
+
+
+
+
 function generaNumeri(min, max) {
   var numero = Math.floor(Math.random() * (max - min)) + min;
   return numero
 }
 
-var arrayNumeriCasuali = [];
-console.log(arrayNumeriCasuali);
 
-for (var i = 1; i <= 16; i++) {
-  numeroCasuale = generaNumeri(1, 100);
-  arrayNumeriCasuali.push(numeroCasuale);
+function controlloNumeroGiaPresente(numero, array) {
+  var result = false;
+  for (var i = 0; i < array.length; i++) {
+    if (numero != array[i]) {
+      risultato = true;
+    }
+  }
+  return risultato;
 }
