d2e91d0dcc44bc4b123031f493e8ee8984db7aa9
Alessandro <aleneri242@gmail.com>
2020-11-19 22:00:19 +0100

    Nuovo aggiornamento

--- js/script.js
+++ js/script.js
@@ -7,31 +7,30 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+var bombe = [];
 
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-
-var arrayBombe = [];
-console.log(arrayBombe);
-
-for (var i = 1; i <= 16; i++) {
-  numeroCasuale = generaNumeri(1, 100);
-  arrayBombe.push(numeroCasuale);
+while (bombe.length < 16) {
+  numeroCasuale = genNumeroCasuale(100);
+  bombe.push(numeroCasuale);
 }
 
+var controlloNumero = numeroGiaEsistente(numeroCasuale, bombe);
+if (controlloNumero == false) {
+    bombe.push(numeroCasuale);
+}
+console.log(bombe);
 
 
 
+// --------------------------------------------------------------
+// FUNZIONI
 
-
-
-function generaNumeri(min, max) {
-  var numero = Math.floor(Math.random() * (max - min)) + min;
-  return numero
+function genNumeroCasuale(max) {
+  return Math.floor(Math.random() * max) + 1;
 }
 
-
-function controlloNumeroGiaPresente(numero, array) {
-  var result = false;
+function numeroGiaEsistente(numero, array) {
+  var risultato = false;
   for (var i = 0; i < array.length; i++) {
     if (numero != array[i]) {
       risultato = true;
